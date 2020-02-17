0dd786de056fbc9fb049beb08a2322b48f446761
Carolina Cordero <ccalvarez@gmail.com>
2020-02-17 12:51:56 -0600

    feat: muestra lista de participantes en una unordered list

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
+    <ul>
+      <li v-for="participante in participantes">
+        <img :src="getIdenticonURL(participante.github)" alt />
+        {{ participante.name }}
+      </li>
+    </ul>
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import participantes from "@/assets/data/participantes.json";
 
 export default {
   name: "App",
+  data: function() {
+    return {
+      participantes: participantes
+    };
+  },
   components: {
     HelloWorld
+  },
+  methods: {
+    getIdenticonURL(username) {
+      return `https://github.com/identicons/${username}.png`;
+    }
   }
 };
 </script>
